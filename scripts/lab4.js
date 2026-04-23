487ccba7ff1cc47d76025058566a5c8cd5d938a7
Emily Nguyen <emn008@ucsd.edu>
2026-04-22 22:47:45 -0700

    fix bugs in script 2

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -6,6 +6,10 @@
  * @returns The sum of the two numbers if add is true and false otherwise.
  */
 function sumValues(num1, num2, add) {
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return false;
+    }
+
     if(add){
         return num1 + num2;
     }
@@ -19,10 +23,10 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
-    if (prices.length === 0) {
+    if (!Array.isArray(prices) || prices.length === 0) {
         return false;
     }
-    
+
     const discounted = []
     const length = prices.length;
     let discountedPrice = 0
